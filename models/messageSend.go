6613ab4fdadae966f89a32bc28eab3ca3d29a30d
Johannes Z. Vollan <johannzv@stud.ntnu.no>
2017-08-03 00:08:32 +0200

    cganged stuff

--- models/messageSend.go
+++ models/messageSend.go
@@ -7,9 +7,18 @@ type MessageSend struct {
 	SenderAction string             `json:"sender_action,omitempty"`
 }
 
+//TODO
+type MessageSendTopicMenu struct {
+	Recipient Agent           `json:"recipient"`
+	Message   MessageTemplate `json:"message,omitempty"`
+}
+
 //MessageSendingType sendingMessage
 type MessageSendingType struct {
 	Text         string        `json:"text,omitempty"`
-	Attachment   Attachment    `json:"attachment,omitempty"`
 	QuickReplies []QuickReplay `json:"quick_replies,omitempty"`
+}
+
+type MessageTemplate struct {
+	Attachment Attachment `json:"attachment,omitempty"`
 }
--- models/send.go
+++ models/send.go
@@ -24,3 +24,16 @@ func (msg MessageSend) Send(token string) (*http.Response, error) {
 	fmt.Println(err2)
 	return res, err2
 }
+
+func (msg MessageSendTopicMenu) Send(token string) (*http.Response, error) {
+	body := new(bytes.Buffer)
+	err := json.NewEncoder(body).Encode(msg)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	res, err2 := http.Post("https://graph.facebook.com/v2.6/me/messages?access_token="+token, "application/json", body)
+	fmt.Println(err2)
+
+	return res, err2
+}
