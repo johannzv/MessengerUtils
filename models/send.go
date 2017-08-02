4dd2217dff6fe6a44caeee22ff9ab5fa2dea2bd4
Johannes Z. Vollan <johannzv@stud.ntnu.no>
2017-08-02 18:08:11 +0200

    stuff22

--- models/send.go
+++ models/send.go
@@ -3,6 +3,7 @@ package models
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -16,8 +17,10 @@ func (msg MessageSend) Send(token string) (*http.Response, error) {
 	body := new(bytes.Buffer)
 	err := json.NewEncoder(body).Encode(msg)
 	if err != nil {
+		fmt.Println(err)
 		return nil, err
 	}
 	res, err2 := http.Post("https://graph.facebook.com/v2.6/me/messages?access_token="+token, "application/json", body)
+	fmt.Println(err2)
 	return res, err2
 }
