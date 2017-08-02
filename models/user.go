765a28f5332cea7154a7730070d1f0ede0166fe6
Johannes Z. Vollan <johannzv@stud.ntnu.no>
2017-08-02 15:52:15 +0200

    added locale

--- models/user.go
+++ models/user.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
 
 // User holds the user object
 type User struct {
@@ -20,4 +24,33 @@ type UserMinimal struct {
 	LastName  string    `json:"last_name,omitempty"`
 	Locale    string    `json:"locale,omitempty"`
 	Created   time.Time `json:"created,omitempty"`
+}
+
+type ExtraInfo struct {
+	FirstName string `json:"first_name,omitempty"`
+	LastName  string `json:"last_name,omitempty"`
+	Locale    string `json:"locale,omitempty"`
+}
+
+//UserInfo interface for fetching parts
+type UserInfo interface {
+	Find(token string) error
+}
+
+//Find sumplements the extra fileds needed for minimal user
+func (user *UserMinimal) Find(token string) error {
+	var extrainfo ExtraInfo
+	res, err := http.Get("https://graph.facebook.com/v2.6/" + user.PSID + "?fields=first_name,last_name,locale&access_token=" + token)
+	if err != nil {
+		return err
+	}
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&extrainfo)
+	if err != nil {
+		return err
+	}
+	user.FirstName = extrainfo.FirstName
+	user.LastName = extrainfo.LastName
+	user.Locale = extrainfo.Locale
+	return err
 }
